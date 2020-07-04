d0f5a6a68ef2a6fe66e1e13c192d905e891ad0c7
hmalak <hmalak@spartaglobal.com>
2020-07-04 12:26:12 +0100

    response

--- oppp.py
+++ oppp.py
@@ -5,10 +5,15 @@ response_bbc = requests.get("https://www.bbc.co.uk")
 
 def check_response_code():
     if response_bbc.status_code == 200:
-        print("Success")
+        print("Success" + " " + response_bbc.url)
     elif response_bbc.status_code == 400:
         print("page not found")
     elif response_bbc.status_code == 404:
         print("something went wrong")
     else:
         print("error code not found")
+
+
+r = requests.post("https://www.github.com", data = {'key':'value'})
+
+print(r)
